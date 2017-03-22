70eb5a73d058ae69ec59b9eb41dada1538174282
huanghuiqi <huiqihuang@126.com>
2017-03-22 18:57:40 +0800

    存放所有跟目录相关的操作

--- dir.func.php
+++ dir.func.php
@@ -1,7 +1,7 @@
 <!--存放所有跟目录相关的操作-->
 <?php
 
-//写一个读目录的函数 有文件 有目录
+//写一个读目录的函数 有文件 有目录 只读取最外层
 function readDirectory($path){
     //打开目录
     $handle = opendir($path);
@@ -9,16 +9,17 @@ function readDirectory($path){
         //.和..这两个特殊目录得先排除 因为做不了任何操作
         if($item!='.' && $item!='..'){
             if(is_file($path.'/'.$item)){
-                $arr['file'][] = $item;
+                $arr['file'][] = $item ;
             }
             if(is_dir($path.'/'.$item)){
-                $arr['dir'][] = $item;
+                $arr['dir'][] = $item ;
             }
         }
     };
     closedir($handle);
     return $arr;
 }
+
 $path='file';
-print_r(readDirectory($path));
+//print_r(readDirectory($path));
 ?>
